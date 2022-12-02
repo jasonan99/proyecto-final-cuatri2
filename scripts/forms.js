b47e043774b754b43ae0368316f1c8ce605bd0ac
jasonan99 <106450277+jasonan99@users.noreply.github.com>
2022-12-02 14:12:58 -0600

    feat(files): add scripts folder

---
+++ scripts/forms.js
@@ -0,0 +1,14 @@
+function openFormL() {
+    document.getElementById("form-log-in").style.display = "block";
+    document.getElementById("form-sign-up").style.display = "none";
+  }
+  function closeFormL() {
+    document.getElementById("form-log-in").style.display = "none";
+  }
+  function openFormS() {
+    document.getElementById("form-sign-up").style.display = "block";
+    document.getElementById("form-log-in").style.display = "none";
+  }
+  function closeFormS() {
+    document.getElementById("form-sign-up").style.display = "none";
+  }

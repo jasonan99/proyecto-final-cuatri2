4f3294d27068b6b5941090d8bf23038f6d2e6c95
jasonan99 <106450277+jasonan99@users.noreply.github.com>
2022-12-08 17:14:57 -0600

    feat(cart.scss): add media querries

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -10,15 +10,14 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
   let slides = document.getElementsByClassName("slideshow");
   let dots = document.getElementsByClassName("slideshow__dots--pass");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
--- scripts/suma.js
+++ scripts/suma.js
@@ -1,10 +1,10 @@
-let contador = 0;
+let contador = 1;
 
 document.querySelector("#suma").addEventListener("click",sumar);
 document.querySelector("#resta").addEventListener("click",restar);
 
 function sumar() {
-    contador = contador+1
+    contador += 1
     if(contador<=0) contador = 0
     document.querySelector("#quantity").innerHTML = contador
 }
@@ -15,13 +15,13 @@ function restar() {
     document.querySelector("#quantity").innerHTML = contador
 }
 
-let contador1 = 0;
+let contador1 = 1;
 
 document.querySelector("#suma1").addEventListener("click",sumar1);
 document.querySelector("#resta1").addEventListener("click",restar1);
 
 function sumar1() {
-    contador1 = contador1+1
+    contador1 += 1
     if(contador1<=0) contador1 = 0
     document.querySelector("#quantity1").innerHTML = contador1
 }
